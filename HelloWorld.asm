f9a597f1d3ddaa9ca52030116823f0b33c210cf9
RawrRadioMouse <55473161+RawrRadioMouse@users.noreply.github.com>
2020-04-27 09:23:30 +0930

    Update HelloWorld.asm

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -52,7 +52,43 @@ section .data
 ;the instruction EG if the instruction was
 ;"mov    ecx,0x80490a4" (in our program this was defined as mov ecx, message, when it runs in assembly it looks
 ;at the memory address where our "message" variable is stored,when disassembling, we can confirm conents of message with x/s <memaddress>),
-; then we should see that memory address
+;then we should see that memory address in ecx (a pointer for data)
+;
+;after continuing this we eventually reach our interrupt instruction, the next time we stepi we will get our ptinter output
+;bonus knowledge - an interrupt instruction is called as such as it "interrupts" the program flow and goes and does something real quick 
+;"You can think of it as like signaling Batman. You need something done, you send the signal, and then he comes to the rescue"
+;(gdb) i r
+;eax            0x4	4
+;ecx            0x80490a4	134516900
+;edx            0xc	12
+;ebx            0x1	1
+;esp            0xbffff3d0	0xbffff3d0
+;ebp            0x0	0x0
+;esi            0x0	0
+;edi            0x0	0
+;eip            0x8048094	0x8048094 <_start+20>
+;eflags         0x202	[ IF ]
+;cs             0x73	115
+;ss             0x7b	123
+;ds             0x7b	123
+;es             0x7b	123
+;fs             0x0	0
+;gs             0x0	0
+;(gdb) disassemble 
+;Dump of assembler code for function _start:
+;   0x08048080 <+0>:	mov    eax,0x4
+;   0x08048085 <+5>:	mov    ebx,0x1
+;   0x0804808a <+10>:	mov    ecx,0x80490a4
+;   0x0804808f <+15>:	mov    edx,0xc
+;=> 0x08048094 <+20>:	int    0x80
+;   0x08048096 <+22>:	mov    eax,0x1
+v   0x0804809b <+27>:	mov    ebx,0x5
+v   0x080480a0 <+32>:	int    0x80
+;End of assembler dump.
+;(gdb) 
+;(gdb) stepi
+;Hello World!0x08048096 in _start ()
+;(gdb) 
 
 
 
